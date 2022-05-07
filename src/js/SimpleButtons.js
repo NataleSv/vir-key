e4df78e51bdcb874cab8d8d1176ce8de9d5f8a8d
Natalia Sorokina <nalya92@list.ru>
2022-05-07 18:09:45 +0300

    fix: edit class SimpleButtons

--- src/js/SimpleButtons.js
+++ src/js/SimpleButtons.js
@@ -1,14 +1,16 @@
 export class SimpleButtons {
-    constructor(value, valueShift, className) {
+    constructor(value, className, codeKey) {
         this.value = value;
-        this.valueShift = valueShift;
         this.className = className;
+        this.codeKey = codeKey;
     }
     createButton() {
         let btn = document.createElement('div');
         btn.classList.add('btn');
         btn.classList.add(this.className);
         btn.innerHTML = this.value;
+        btn.id = this.codeKey;
+        btn.setAttribute("data-code", this.codeKey);
         return btn;
     }
 
